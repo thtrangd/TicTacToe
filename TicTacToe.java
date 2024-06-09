cc9d971d9862d54201da804fd1d5a8f0d263ed5d
thtrangd <162159111+thtrangd@users.noreply.github.com>
2024-06-09 18:11:45 +0700

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,17 +3,15 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class TicTacToe {
-    int boardWidth = 700;
-    int boardHeight = 750; 
-    
+    int boardWidth = 800;
+    int boardHeight = 850; 
 
-    JFrame frame = new JFrame("Tic-Tac-Toe");
+    JFrame frame = new JFrame("3SIS Tic-Tac-Toe");
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
     JPanel boardPanel = new JPanel();
     JPanel bottomPanel = new JPanel();
 
-    
     JButton[][] board = new JButton[3][3];
     Player playerX;
     Player playerO;
@@ -21,11 +19,11 @@ public class TicTacToe {
 
     boolean gameOver = false;
     int turns = 0;
+    int playerXWins = 0;
+    int playerOWins = 0;
+    JLabel scoreLabel;
 
-    public static void main(String[] args) {
-        new TicTacToe();
-    }
-
+    // Constructor to initialize the TicTacToe game
     TicTacToe() {
         frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
@@ -33,9 +31,7 @@ public class TicTacToe {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-        
-    
-    
+
         showWelcomeScreen();
     }
 
@@ -58,8 +54,6 @@ public class TicTacToe {
             }
         });
         welcomePanel.add(playWithHumanButton);
-    
-
 
         JButton playWithAIButton = new JButton("Robot (๑˃̵ᴗ˂̵)ﻭ");
         playWithAIButton.setFont(new Font("Monaco", Font.BOLD, 30));
@@ -80,30 +74,32 @@ public class TicTacToe {
         this.playerX = playerX;
         this.playerO = playerO;
         this.currentPlayer = playerX;
-
+    
         frame.getContentPane().removeAll();
         frame.repaint();
     
-
-        //textLabel.setBackground(new Color(255, 192, 203)); // Pink
-        //textLabel.setForeground(new Color(128, 0, 128));   // Purple
         textLabel.setFont(new Font("Monaco", Font.BOLD, 50));
         textLabel.setHorizontalAlignment(JLabel.CENTER);
         textLabel.setText("Let's go, " + currentPlayer.getSymbol());
-        //textLabel.setForeground(new Color(204, 153, 255));
         textLabel.setOpaque(true);
-
+    
         textPanel.setLayout(new BorderLayout());
-        textPanel.add(textLabel);
+        scoreLabel = new JLabel("X: " + playerXWins + "  O: " + playerOWins, JLabel.CENTER);
+        scoreLabel.setFont(new Font("Monaco", Font.BOLD, 30));
+        scoreLabel.setForeground(new Color(78, 78, 223));
+        textPanel.add(scoreLabel, BorderLayout.NORTH);
+        textPanel.add(textLabel, BorderLayout.CENTER);
         frame.add(textPanel, BorderLayout.NORTH);
-
+    
         boardPanel.setLayout(new GridLayout(3, 3));
-        boardPanel.setBackground(new Color(253, 195, 244));
+        boardPanel.setBackground(new Color(204, 204, 255));
         frame.add(boardPanel, BorderLayout.CENTER);
-
+    
         bottomPanel.setLayout(new FlowLayout());
+    
         JButton playAgainButton = new JButton("Play Again");
         playAgainButton.setFont(new Font("Monaco", Font.BOLD, 30));
+        playAgainButton.setForeground(new Color(204, 103, 255));
         playAgainButton.setVisible(false);
         playAgainButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -111,19 +107,30 @@ public class TicTacToe {
             }
         });
         bottomPanel.add(playAgainButton);
-        frame.add(bottomPanel, BorderLayout.SOUTH);
 
+        JButton endButton = new JButton("End");
+        endButton.setFont(new Font("Monaco", Font.BOLD, 30));
+        endButton.setForeground(new Color(204, 103, 255));
+        endButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        bottomPanel.add(endButton);
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+    
         for (int r = 0; r < 3; r++) {
             for (int c = 0; c < 3; c++) {
                 JButton tile = new JButton();
                 board[r][c] = tile;
                 boardPanel.add(tile);
-
-                textLabel.setBackground(new Color(253, 195, 244)); // Pink
-                textLabel.setForeground(new Color(78, 78, 223));
+    
+                textLabel.setBackground(new Color(204, 204, 255)); // Color of textLabel
+                textLabel.setForeground(new Color(78, 78, 223));   // Color of "Let's go, X, Y"
                 tile.setFont(new Font("Monaco", Font.BOLD, 120));
+                tile.setForeground(new Color(204, 103, 255));     // Set color to pink for X and O
                 tile.setFocusable(false);
-
+    
                 tile.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
                         if (gameOver) return;
@@ -148,10 +155,11 @@ public class TicTacToe {
                 });
             }
         }
-
+    
         frame.revalidate();
         frame.repaint();
     }
+    
 
     private void resetGame() {
         turns = 0;
@@ -162,17 +170,15 @@ public class TicTacToe {
         for (int r = 0; r < 3; r++) {
             for (int c = 0; c < 3; c++) {
                 board[r][c].setText("");
-                board[r][c].setBackground(Color.pink);
-                board[r][c].setForeground(new Color(204, 103, 255));    
-                 }
+                board[r][c].setForeground(new Color(204, 103, 255));     // Color of X and O when reset again
+            }
         }
     
         bottomPanel.getComponent(0).setVisible(false); // Hide play again button
+        scoreLabel.setText("X: " + playerXWins + "  O: " + playerOWins); // Update score on reset
         frame.revalidate();
         frame.repaint();
     }
-    
-
 
     void switchPlayer() {
         currentPlayer = currentPlayer == playerX ? playerO : playerX;
@@ -191,6 +197,12 @@ public class TicTacToe {
                 }
                 gameOver = true;
                 bottomPanel.getComponent(0).setVisible(true); // Show play again button
+                if (board[r][0].getText().equalsIgnoreCase("x")) {
+                    playerXWins++;
+                } else {
+                    playerOWins++;
+                }
+                scoreLabel.setText("X: " + playerXWins + "  O: " + playerOWins); // Update score on win
                 return;
             }
         }
@@ -206,6 +218,12 @@ public class TicTacToe {
                 }
                 gameOver = true;
                 bottomPanel.getComponent(0).setVisible(true); // Show play again button
+                if (board[0][c].getText().equalsIgnoreCase("x")) {
+                    playerXWins++;
+                } else {
+                    playerOWins++;
+                }
+                scoreLabel.setText("X: " + playerXWins + "  O: " + playerOWins); // Update score on win
                 return;
             }
         }
@@ -219,41 +237,41 @@ public class TicTacToe {
             }
             gameOver = true;
             bottomPanel.getComponent(0).setVisible(true); // Show play again button
+            if (board[0][0].getText().equalsIgnoreCase("x")) {
+                playerXWins++;
+            } else {
+                playerOWins++;
+            }
+            scoreLabel.setText("X: " + playerXWins + "  O: " + playerOWins); // Update score on win
             return;
         }
 
-        // anti-diagonally
-        if (board[0][2].getText().equals(board[1][1].getText()) &&
-            board[1][1].getText().equals(board[2][0].getText()) &&
-            !board[0][2].getText().equals("")) {
-            setWinner(board[0][2]);
-            setWinner(board[1][1]);
-            setWinner(board[2][0]);
+        if (board[2][0].getText().equals(board[1][1].getText()) &&
+            board[1][1].getText().equals(board[0][2].getText()) &&
+            !board[2][0].getText().equals("")) {
+            for (int i = 0; i < 3; i++) {
+                setWinner(board[2 - i][i]);
+            }
             gameOver = true;
             bottomPanel.getComponent(0).setVisible(true); // Show play again button
+            if (board[2][0].getText().equalsIgnoreCase("x")) {
+                playerXWins++;
+            } else {
+                playerOWins++;
+            }
+            scoreLabel.setText("X: " + playerXWins + "  O: " + playerOWins); // Update score on win
             return;
         }
 
         if (turns == 9) {
-            for (int r = 0; r < 3; r++) {
-                for (int c = 0; c < 3; c++) {
-                    setTie(board[r][c]);
-                }
-            }
+            textLabel.setText("It's a tie!");
             gameOver = true;
             bottomPanel.getComponent(0).setVisible(true); // Show play again button
         }
     }
 
-    void setWinner(JButton tile) {
-        tile.setForeground(new Color(255, 0, 127));
-        tile.setBackground(Color.gray);
-        textLabel.setText(currentPlayer.getSymbol() + "-my champion ٩(♡ε♡ )۶");
-    }
-
-    void setTie(JButton tile) {
-        tile.setForeground(new Color(255, 102, 255));
-        tile.setBackground(Color.gray);
-        textLabel.setText("It's a draw (~_~メ)");
+    void setWinner(JButton button) {
+        button.setForeground(new Color(255, 0, 0)); // Winner's color is red
+        textLabel.setText(currentPlayer.getSymbol() + " - my champion ٩(♡ε♡ )۶");
     }
 }
